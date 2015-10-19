c9f92c7fa3f32e37c56fb2b4a84dd6abad377ad6
andy cao <cao_huayu@hotmail.com>
2015-10-19 18:41:20 +0800

    update

--- 11/app/scripts/main.js
+++ 11/app/scripts/main.js
@@ -1,2 +1,30 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+(function($){
+  $('.collapse').on('hide.bs.collapse', function () {
+    var toggle = $(this).parent().find(".arrow-down").addClass("arrow-right").removeClass("arrow-down");
+    console.log(this);
+  });
+  $('.collapse').on('show.bs.collapse', function () {
+    var toggle = $(this).parent().find(".arrow-right").addClass("arrow-down").removeClass("arrow-right");
+    console.log('show');
+  });
+
+  $("#main .rule").click(function(){
+    location.href = "rule.html";
+  });
+
+
+  $("#citys").click(function(){
+    $('#myModal').modal('show');
+  });
+
+  $("#myModal .modal-body .luoyang").click(function(){
+    console.log('luoyang');
+  });
+  $("#myModal .modal-body .haikou").click(function(){
+    console.log('haikou');
+  });
+  $("#myModal .modal-body .zhengzhou").click(function(){
+    console.log('zhengzhou');
+  });
+  })(jQuery);
 
